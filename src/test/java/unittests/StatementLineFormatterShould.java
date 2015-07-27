7e1847495a6820e1d582bf9dc40c67f40bfccc99
trikitrok <trikitrok@gmail.com>
2015-07-27 23:19:45 +0200

    Reformatted code

--- src/test/java/unittests/StatementLineFormatterShould.java
+++ src/test/java/unittests/StatementLineFormatterShould.java
@@ -15,10 +15,7 @@ public class StatementLineFormatterShould {
     public void format_a_statement_line() {
         StatementLineFormatter statementLineFormatter = new StatementLineFormatter();
 
-        assertThat(
-                statementLineFormatter.format(statementLine(500, date(10, 4, 2014), 1500)),
-                is("10/04/2014 | 500.00 | 1500.00")
-        );
+        assertThat(statementLineFormatter.format(statementLine(500, date(10, 4, 2014), 1500)),
+                is("10/04/2014 | 500.00 | 1500.00"));
     }
-
 }
---
+++ src/test/java/unittests/helpers/TransactionFactory.java
@@ -0,0 +1,7 @@
+package unittests.helpers;
+
+/**
+ * Created by trikitrok on 27/07/15.
+ */
+public class TransactionFactory {
+}
